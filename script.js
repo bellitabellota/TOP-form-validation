cca4baf9368ac9520843598e3ae1d70740863bf3
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-02-25 14:30:10 +0000

    Change 'blur' events to 'input', finalize password and passwordConfirmation EventListener and add EventListener for submit button

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const inputs = document.querySelectorAll("input");
 const password = document.getElementById("password");
 const passwordConfirmation = document.getElementById("password-confirmation");
+const submitButton = document.querySelector(".submit");
+const formContainer = document.querySelector("form");
 
 password.addEventListener("input", () => {
   password.setCustomValidity("");
@@ -11,6 +13,8 @@ password.addEventListener("input", () => {
   if(! (/[A-Z]/.test(password.value) && /\d/.test(password.value) && /[#\+]/.test(password.value))) {
     password.setCustomValidity("Password must contain at least 1 digit, 1 uppercase letter and a + or # symbol.");
   }
+
+  password.reportValidity();
 });
 
 passwordConfirmation.addEventListener("input", () => {
@@ -22,10 +26,26 @@ passwordConfirmation.addEventListener("input", () => {
   if(! (password.value === passwordConfirmation.value)) {
     passwordConfirmation.setCustomValidity("Password Confirmation must match password.");
   }
+
+  passwordConfirmation.reportValidity();
 });
 
-inputs.forEach(( input) => {
-  input.addEventListener("blur", () => {
+inputs.forEach((input, inputIndex) => {
+  input.addEventListener("input", () => {
+    if (inputIndex === (inputs.length - 1) || (inputs.length - 2)) {
+      return
+    }
+
     input.reportValidity();
   })
 });
+
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault();
+ if(!formContainer.checkValidity()) {
+
+  return formContainer.reportValidity();
+ }
+
+ alert("Successful form submission!");
+})
